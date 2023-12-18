dcb4d3919e68d865e8ea196ed8581a07283b38eb
Shreya Maheshwari <shreya.maheshwari.2000@gmail.com>
2023-12-18 12:06:31 +0530

    input

--- src/(components)/TopNav.js
+++ src/(components)/TopNav.js
@@ -11,6 +11,7 @@ export default function TopNav() {
   const isLandingPage = pathname === "/";
   const [inputValue, setInputValue] = useState("");
 
+  console.log("####", inputValue);
   return (
     <nav
       className={clsx(
@@ -41,19 +42,6 @@ export default function TopNav() {
           >
             Our Reviews
           </Link>
-          {/* <div className="flex gap-2">
-            <input
-              className="rounded border border-gray-300 px-2" // Added border and adjusted padding
-              onChange={(e) => {
-                setInputValue(e.target.value);
-              }}
-              placeholder="Insert Url"
-              style={{ color: "black" }} // Set the text color to black
-            />
-            <button className="bg-white rounded-md text-blue-600 font-semibold px-2 hover:shadow-lg shadow text-xs border border-blue-600 hover:bg-blue-600 hover:text-white">
-              Connect Store
-            </button>
-          </div> */}
           <div className="flex gap-2">
             <input
               className="rounded border border-gray-300 px-3 focus:outline-none focus:border-blue-500 text-black"
@@ -63,7 +51,7 @@ export default function TopNav() {
             />
             <Link
               className="bg-white text-black rounded-md px-4 py-2 font-semibold border border-gray-300 hover:bg-gray-100 transition duration-300 ease-in-out"
-              href={`https://${inputValue}.myshopify.com/admin/oauth/authorize?client_id=570095e20b1937221e5c687aaee9797e&redirect_uri=https%3A%2F%2F7331-2405-201-5011-217a-48d1-12a4-d20d-bb7a.ngrok-free.app%2Freview-requests%2Foauth%2Fcallback&scope=read_products,read_orders,read_analytics,read_orders,read_product_feeds,read_product_listings,read_products`}
+              href={`https://test-review-ap.myshopify.com/admin/oauth/authorize?client_id=4934c58427211856cfe456e5b638d4fc&scope=read_products,read_orders,read_analytics,read_orders,read_product_feeds,read_product_listings,read_products&redirect_uri=https://17db-2401-4900-1c54-4d84-dde2-a72-3f7-926.ngrok-free.app/review-requests/oauth/callback`}
             >
               Connect Store
             </Link>
